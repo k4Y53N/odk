8bc83955aae9c72280dd2d067e42b82165499c91
k4Y53N <ks93041@gmail.com>
2026-04-14 14:49:29 +0800

    add configer.py

---
+++ odk/detector/configer.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+from enum import Enum
+
+__all__ = [
+    'ModelConfiger',
+    'Version',
+    'ObjectDetectConfiger',
+]
+
+
+@dataclass
+class ModelConfiger:
+    weight_path: str
+    shape: list[list[int | None]] | None
+
+
+class Version(str, Enum):
+    V4 = 'v4'
+    V7 = 'v7'
+    V8 = 'v8'
+    V9 = 'v9'
+    V11 = 'v11'
+
+    def __str__(self) -> str:
+        return self.value
+
+
+@dataclass
+class ObjectDetectConfiger(ModelConfiger):
+    version: Version
+    class_label: list[str]
+    shape: list[list[int | None]] | None = None
