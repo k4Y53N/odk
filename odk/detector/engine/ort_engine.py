41f7f85609505d9ef545bd013a3d66b0f8c92647
k4Y53N <ks93041@gmail.com>
2026-04-14 17:04:03 +0800

    add ort_engine.py

---
+++ odk/detector/engine/ort_engine.py
@@ -0,0 +1,52 @@
+import numpy as np
+import onnxruntime as ort
+from numpy.typing import DTypeLike, NDArray
+
+from ..configer import ModelConfiger
+from .engine import Engine
+
+DTYPE_MAP: dict[str, DTypeLike] = {
+    'tensor(float)': np.float32,
+    'tensor(float16)': np.float16,
+    'tensor(uint8)': np.uint8,
+}
+
+
+class OrtEngine(Engine):
+    def __init__(self, session: ort.InferenceSession):
+        self.session = session
+        self.input_names = tuple[str](
+            model_input.name for model_input in self.session.get_inputs()
+        )
+
+    @classmethod
+    def load(cls, configer: ModelConfiger) -> 'OrtEngine':
+        return cls(
+            ort.InferenceSession(
+                path_or_bytes=configer.weight_path,
+                providers=['CUDAExecutionProvider', 'CPUExecutionProvider'],
+            )
+        )
+
+    def infer(self, input_tensors: tuple[NDArray, ...]) -> tuple[NDArray, ...]:
+        input_feed = {
+            input_name: input_tensor
+            for input_name, input_tensor in zip(self.input_names, input_tensors)
+        }
+
+        return tuple(self.session.run(None, input_feed))
+
+    @property
+    def input_shapes(self) -> tuple[tuple[int | None, ...], ...]:
+        return tuple(
+            tuple(
+                shape if isinstance(shape, int) else -1 for shape in model_input.shape
+            )
+            for model_input in self.session.get_inputs()
+        )
+
+    @property
+    def input_dtypes(self) -> tuple[DTypeLike, ...]:
+        return tuple(
+            DTYPE_MAP[model_input.type] for model_input in self.session.get_inputs()
+        )
