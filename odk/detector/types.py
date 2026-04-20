687259e90f43f160daea6aa24383215762ee0ec0
k4Y53N <ks93041@gmail.com>
2026-04-20 11:44:22 +0800

    add ConfigT type

--- odk/detector/types.py
+++ odk/detector/types.py
@@ -1,9 +1,13 @@
 from typing import TypeVar
 
+from .configer import ModelConfiger
+
 __all__ = [
+    'ConfigT',
     'OptionT',
     'ResultT',
 ]
 
+ConfigT = TypeVar('ConfigT', bound=ModelConfiger)
 OptionT = TypeVar('OptionT')
 ResultT = TypeVar('ResultT')
