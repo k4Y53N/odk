3ae5ab2894c22ef567edf35f27473997b85f1667
k4Y53N <51146583+k4Y53N@users.noreply.github.com>
2026-04-27 16:41:01 +0800

    long doc line break

--- odk/tracker/sort/kalman_filter.py
+++ odk/tracker/sort/kalman_filter.py
@@ -9,10 +9,11 @@ __all__ = [
 
 class KalmanFilter:
     """Kalman filtering, also known as linear quadratic estimation (LQE), is an
-    algorithm that uses a series of measurements
-    observed over time, containing statistical noise and other inaccuracies,
-    and produces estimates of unknown variables that tend to be more accurate than those based on a single measurement
-    alone, by estimating a joint probability distribution over the variables for each time frame.
+    algorithm that uses a series of measurements observed over time, containing
+    statistical noise and other inaccuracies, and produces estimates of unknown
+    variables that tend to be more accurate than those based on a single measurement
+    alone, by estimating a joint probability distribution over the variables for each
+    time frame.
     """
 
     def __init__(self, dim_x: int, dim_z: int):
