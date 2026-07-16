9a987a53bc30c995be5e15fe18a50bb62232ee6f
k4Y53N <51146583+k4Y53N@users.noreply.github.com>
2026-07-16 11:32:33 +0800

    add InputT type

--- odk/detector/types.py
+++ odk/detector/types.py
@@ -5,9 +5,11 @@ from .configer import ModelConfiger
 __all__ = [
     'ConfigT',
     'ParamsT',
+    'InputT',
     'ResultT',
 ]
 
 ConfigT = TypeVar('ConfigT', bound=ModelConfiger)
 ParamsT = TypeVar('ParamsT')
+InputT = TypeVar('InputT')
 ResultT = TypeVar('ResultT')
